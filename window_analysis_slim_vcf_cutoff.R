7dc7e1bda4099ec062e0870b09477a299f76f983
bfraser-commits <63240617+bfraser-commits@users.noreply.github.com>
2025-08-05 08:36:27 +0100

    Add files via upload

--- window_analysis_slim_vcf_cutoff.R
+++ window_analysis_slim_vcf_cutoff.R
@@ -26,6 +26,7 @@ nrow(TA_outs_seltot)
 (nrow(TA_outs_seltot)/nrow(TA_obs))*100
 
 
+
 #CA
 CA <- read.table("Documents/FIBR_temporal/slims_test/CA_vcf_slim_winds.txt", header=T)
 summary(CA)
@@ -53,8 +54,6 @@ nrow(CA_outs_seltot)
 
 #UL
 UL <- read.table("Documents/FIBR_temporal/slims_test/UL_vcf_slim_winds.txt", header=T)
-summary(UL)
-summary(TA)
 UL <- subset(UL, site_count < 1000)
 
 ULq1 <- quantile(UL$mean_sel1, 0.95, na.rm = TRUE)
@@ -79,8 +78,6 @@ nrow(UL_outs_seltot)
 
 #LL
 LL <- read.table("Documents/FIBR_temporal/slims_test/LL_vcf_slim_winds.txt", header=T)
-summary(LL)
-summary(TA)
 LL <- subset(UL, site_count < 1000)
 
 LLq1 <- quantile(LL$mean_sel1, 0.95, na.rm = TRUE)
@@ -102,6 +99,286 @@ nrow(LL_outs_sel2)
 LL_outs_seltot <- subset(LL_obs, mean_seltot > LLqtot)
 nrow(LL_outs_seltot)
 (nrow(LL_outs_seltot)/nrow(LL_obs))*100
+
+###sup figures
+head(TA)
+TA_obs_tmp <- data.frame(mean_sel1 = TA_obs$mean_sel1, mean_sel2 = TA_obs$mean_sel2, mean_seltot = TA_obs$mean_seltot, dataset= "observed")
+TA_slim_tmp <-  data.frame(mean_sel1 = TA$mean_sel1, mean_sel2 = TA$mean_sel2, mean_seltot = TA$mean_seltot, dataset= "simulated")
+
+TA_final_df <- data.frame(rbind(TA_obs_tmp, TA_slim_tmp))
+head(final_df)
+
+
+sel_1_fig <- ggplot(TA_final_df, aes(x = dataset, y = mean_sel1, fill = dataset)) +
+  geom_violin(alpha = 0.7, position = position_dodge(width = 0.8)) +
+  scale_fill_manual(values = c("observed" = "steelblue", "simulated" = "orange")) +
+  geom_hline(yintercept = q1, linetype = "dashed", color = "darkgrey")+
+  labs(
+    title = "Period 1",
+    x = "",
+    y = "Mean delta AF",
+    fill = "Dataset",
+  ) +
+  theme_minimal()+
+  theme(legend.position = "none")
+
+sel_1_fig
+
+sel_2_fig <- ggplot(TA_final_df, aes(x = dataset, y = mean_sel2, fill = dataset)) +
+  geom_violin(alpha = 0.7, position = position_dodge(width = 0.8)) +
+  scale_fill_manual(values = c("observed" = "steelblue", "simulated" = "orange")) +
+  geom_hline(yintercept = q2, linetype = "dashed", color = "darkgrey")+
+  labs(
+    title = "Period 2",
+    x = "",
+    y = "Mean delta AF",
+    fill = "Dataset",
+  ) +
+  theme_minimal()+
+  theme(legend.position = "none")
+
+sel_2_fig
+
+
+sel_tot_fig <- ggplot(TA_final_df, aes(x = dataset, y = mean_seltot, fill = dataset)) +
+  geom_violin(alpha = 0.7, position = position_dodge(width = 0.8)) +
+  scale_fill_manual(values = c("observed" = "steelblue", "simulated" = "orange")) +
+  geom_hline(yintercept = qtot, linetype = "dashed", color = "darkgrey")+
+  labs(
+    title = "Total Period",
+    x = "Dataset",
+    y = "Mean delta AF",
+    fill = "Dataset",
+  ) +
+  theme_minimal()+
+  theme(legend.position = "none")
+
+sel_tot_fig
+
+combined_plot <- plot_grid(sel_1_fig, sel_2_fig, sel_tot_fig, ncol=1, labels = c("A", "B", "C"), align = "v")
+
+final_plot <- plot_grid(
+  ggdraw() + 
+    draw_label("TA",  fontface = 'bold', size = 16, x = 0.5, hjust = 0.5), 
+  combined_plot, ncol = 1,  rel_heights = c(0.1, 1.5)  
+)
+
+final_plot
+
+pdf("Documents/FIBR_temporal/slims_test/TA_sup_fig_window.pdf", height = 6, width = 3)
+final_plot
+dev.off()
+
+head(CA)
+CA_obs_tmp <- data.frame(mean_sel1 = CA_obs$mean_sel1, mean_sel2 = CA_obs$mean_sel2, mean_seltot = CA_obs$mean_seltot, dataset= "observed")
+CA_slim_tmp <-  data.frame(mean_sel1 = CA$mean_sel1, mean_sel2 = CA$mean_sel2, mean_seltot = CA$mean_seltot, dataset= "simulated")
+
+CA_final_df <- data.frame(rbind(CA_obs_tmp, CA_slim_tmp))
+head(final_df)
+
+
+CA_sel_1_fig <- ggplot(CA_final_df, aes(x = dataset, y = mean_sel1, fill = dataset)) +
+  geom_violin(alpha = 0.7, position = position_dodge(width = 0.8)) +
+  scale_fill_manual(values = c("observed" = "steelblue", "simulated" = "orange")) +
+  geom_hline(yintercept = CAq1, linetype = "dashed", color = "darkgrey")+
+  labs(
+    title = "Period 1",
+    x = "",
+    y = "Mean delta AF",
+    fill = "Dataset",
+  ) +
+  theme_minimal()+
+  theme(legend.position = "none")
+
+CA_sel_1_fig
+
+CA_sel_2_fig <- ggplot(CA_final_df, aes(x = dataset, y = mean_sel2, fill = dataset)) +
+  geom_violin(alpha = 0.7, position = position_dodge(width = 0.8)) +
+  scale_fill_manual(values = c("observed" = "steelblue", "simulated" = "orange")) +
+  geom_hline(yintercept = CAq2, linetype = "dashed", color = "darkgrey")+
+  labs(
+    title = "Period 2",
+    x = "",
+    y = "Mean delta AF",
+    fill = "Dataset",
+  ) +
+  theme_minimal()+
+  theme(legend.position = "none")
+
+CA_sel_2_fig
+
+
+CA_sel_tot_fig <- ggplot(CA_final_df, aes(x = dataset, y = mean_seltot, fill = dataset)) +
+  geom_violin(alpha = 0.7, position = position_dodge(width = 0.8)) +
+  scale_fill_manual(values = c("observed" = "steelblue", "simulated" = "orange")) +
+  geom_hline(yintercept = CAqtot, linetype = "dashed", color = "darkgrey")+
+  labs(
+    title = "Total Period",
+    x = "Dataset",
+    y = "Mean delta AF",
+    fill = "Dataset",
+  ) +
+  theme_minimal()+
+  theme(legend.position = "none")
+
+CA_sel_tot_fig
+
+CA_combined_plot <- plot_grid(CA_sel_1_fig, CA_sel_2_fig, CA_sel_tot_fig, ncol=1, labels = c("A", "B", "C"), align = "v")
+
+CA_final_plot <- plot_grid(
+  ggdraw() + 
+    draw_label("CA",  fontface = 'bold', size = 16, x = 0.5, hjust = 0.5), 
+  CA_combined_plot, ncol = 1,  rel_heights = c(0.1, 1.5)  
+)
+
+
+
+pdf("Documents/FIBR_temporal/slims_test/CA_sup_fig_window.pdf", height = 6, width = 3)
+CA_final_plot
+dev.off()
+
+
+head(UL)
+UL_obs_tmp <- data.frame(mean_sel1 = UL_obs$mean_sel1, mean_sel2 = UL_obs$mean_sel2, mean_seltot = UL_obs$mean_seltot, dataset= "observed")
+UL_slim_tmp <-  data.frame(mean_sel1 = UL$mean_sel1, mean_sel2 = UL$mean_sel2, mean_seltot = UL$mean_seltot, dataset= "simulated")
+
+UL_final_df <- data.frame(rbind(UL_obs_tmp, UL_slim_tmp))
+head(UL_final_df)
+
+
+UL_sel_1_fig <- ggplot(UL_final_df, aes(x = dataset, y = mean_sel1, fill = dataset)) +
+  geom_violin(alpha = 0.7, position = position_dodge(width = 0.8)) +
+  scale_fill_manual(values = c("observed" = "steelblue", "simulated" = "orange")) +
+  geom_hline(yintercept = ULq1, linetype = "dashed", color = "darkgrey")+
+  labs(
+    title = "Period 1",
+    x = "",
+    y = "Mean delta AF",
+    fill = "Dataset",
+  ) +
+  theme_minimal()+
+  theme(legend.position = "none")
+
+UL_sel_1_fig
+ULq2
+
+UL_sel_2_fig <- ggplot(UL_final_df, aes(x = dataset, y = mean_sel2, fill = dataset)) +
+  geom_violin(alpha = 0.7, position = position_dodge(width = 0.8)) +
+  scale_fill_manual(values = c("observed" = "steelblue", "simulated" = "orange")) +
+  geom_hline(yintercept = ULq2, linetype = "dashed", color = "darkgrey")+
+  labs(
+    title = "Period 2",
+    x = "",
+    y = "Mean delta AF",
+    fill = "Dataset",
+  ) +
+  theme_minimal()+
+  theme(legend.position = "none")
+
+UL_sel_2_fig
+
+ULqtot
+
+UL_sel_tot_fig <- ggplot(UL_final_df, aes(x = dataset, y = mean_seltot, fill = dataset)) +
+  geom_violin(alpha = 0.7, position = position_dodge(width = 0.8)) +
+  scale_fill_manual(values = c("observed" = "steelblue", "simulated" = "orange")) +
+  geom_hline(yintercept = ULqtot, linetype = "dashed", color = "darkgrey")+
+  labs(
+    title = "Total Period",
+    x = "Dataset",
+    y = "Mean delta AF",
+    fill = "Dataset",
+  ) +
+  theme_minimal()+
+  theme(legend.position = "none")
+
+UL_sel_tot_fig
+
+UL_combined_plot <- plot_grid(UL_sel_1_fig, UL_sel_2_fig, UL_sel_tot_fig, ncol=1, labels = c("A", "B", "C"), align = "v")
+
+UL_final_plot <- plot_grid(
+  ggdraw() + 
+    draw_label("UL",  fontface = 'bold', size = 16, x = 0.5, hjust = 0.5), 
+  UL_combined_plot, ncol = 1,  rel_heights = c(0.1, 1.5)  
+)
+
+
+
+pdf("Documents/FIBR_temporal/slims_test/UL_sup_fig_window.pdf", height = 6, width = 3)
+UL_final_plot
+dev.off()
+
+
+head(LL)
+LL_obs_tmp <- data.frame(mean_sel1 = LL_obs$mean_sel1, mean_sel2 = LL_obs$mean_sel2, mean_seltot = LL_obs$mean_seltot, dataset= "observed")
+LL_slim_tmp <-  data.frame(mean_sel1 = LL$mean_sel1, mean_sel2 = LL$mean_sel2, mean_seltot = LL$mean_seltot, dataset= "simulated")
+
+LL_final_df <- data.frame(rbind(LL_obs_tmp, LL_slim_tmp))
+head(LL_final_df)
+
+LLq1
+
+LL_sel_1_fig <- ggplot(LL_final_df, aes(x = dataset, y = mean_sel1, fill = dataset)) +
+  geom_violin(alpha = 0.7, position = position_dodge(width = 0.8)) +
+  scale_fill_manual(values = c("observed" = "steelblue", "simulated" = "orange")) +
+  geom_hline(yintercept = LLq1, linetype = "dashed", color = "darkgrey")+
+  labs(
+    title = "Period 1",
+    x = "",
+    y = "Mean delta AF",
+    fill = "Dataset",
+  ) +
+  theme_minimal()+
+  theme(legend.position = "none")
+
+LL_sel_1_fig
+LLq2
+
+LL_sel_2_fig <- ggplot(LL_final_df, aes(x = dataset, y = mean_sel2, fill = dataset)) +
+  geom_violin(alpha = 0.7, position = position_dodge(width = 0.8)) +
+  scale_fill_manual(values = c("observed" = "steelblue", "simulated" = "orange")) +
+  geom_hline(yintercept = LLq2, linetype = "dashed", color = "darkgrey")+
+  labs(
+    title = "Period 2",
+    x = "",
+    y = "Mean delta AF",
+    fill = "Dataset",
+  ) +
+  theme_minimal()+
+  theme(legend.position = "none")
+
+LL_sel_2_fig
+
+LLqtot
+
+LL_sel_tot_fig <- ggplot(LL_final_df, aes(x = dataset, y = mean_seltot, fill = dataset)) +
+  geom_violin(alpha = 0.7, position = position_dodge(width = 0.8)) +
+  scale_fill_manual(values = c("observed" = "steelblue", "simulated" = "orange")) +
+  geom_hline(yintercept = LLqtot, linetype = "dashed", color = "darkgrey")+
+  labs(
+    title = "Total Period",
+    x = "Dataset",
+    y = "Mean delta AF",
+    fill = "Dataset",
+  ) +
+  theme_minimal()+
+  theme(legend.position = "none")
+
+LL_sel_tot_fig
+
+LL_combined_plot <- plot_grid(LL_sel_1_fig, LL_sel_2_fig, LL_sel_tot_fig, ncol=1, labels = c("A", "B", "C"), align = "v")
+
+LL_final_plot <- plot_grid(
+  ggdraw() + 
+    draw_label("LL",  fontface = 'bold', size = 16, x = 0.5, hjust = 0.5), 
+  LL_combined_plot, ncol = 1,  rel_heights = c(0.1, 1.5)  
+)
+
+
+
+pdf("Documents/FIBR_temporal/slims_test/LL_sup_fig_window.pdf", height = 6, width = 3)
+LL_final_plot
+dev.off()
 
 ##explore chr sel 2
 LL_chr <- subset(LL_obs, chr == "chr20")
@@ -277,6 +554,10 @@ nrow(in_any_2)
 in_only_1 <- subset(id_df, count == 1)
 nrow(in_only_1)
 
+candidate_sel1 <- subset(id_df, count >= 3)
+head(candidate_sel1)
+
+write.table(candidate_sel1, "Documents/FIBR_temporal/slims_test/candidate_window_sel1.txt", sep = "\t", row.names=F)
 ##in sel2
 
 # Combine all IDs
@@ -301,12 +582,16 @@ nrow(in_all_4)
 in_all_4
 in_any_3 <- subset(id_df_2, count == 3)
 nrow(in_any_3)
+in_any_3
 in_any_2 <- subset(id_df_2, count == 2)
 nrow(in_any_2)
 in_only_1 <- subset(id_df_2, count == 1)
 nrow(in_only_1)
 
 head(CA)
+candidate_sel2 <- subset(id_df_2, count >=3)
+nrow(candidate_sel2)
+write.table(candidate_sel2, "Documents/FIBR_temporal/slims_test/candidate_window_sel2.txt", sep = "\t", row.names=F)
 
 # Combine all IDs
 all_seltot_out_ID <- unique(c(CA_outs_seltot$ID, TA_outs_seltot$ID, UL_outs_seltot$ID, LL_outs_seltot$ID))
@@ -336,4 +621,6 @@ nrow(in_any_2)
 in_only_1 <- subset(id_df_tot, count == 1)
 nrow(in_only_1)
 
-
+candidate_regions_total <- subset(id_df_tot, count >= 3)
+nrow(candidate_regions_total)
+write.table(candidate_regions_total, file = "Documents/FIBR_temporal/slims_test/candidate_window_total.txt", sep = "\t", quote=F, row.names=F)
